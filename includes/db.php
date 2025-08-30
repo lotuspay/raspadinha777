516c22c12c3da1709189a922ceacbc404b522fe4
lotuspay <srmilho@proton.me>
2025-08-30 09:43:03 -0300

    db fix

--- includes/db.php
+++ includes/db.php
@@ -3,10 +3,10 @@ ini_set("display_errors", 0);
 ini_set("html_errors", 0);
 error_reporting(E_ALL);
 
-$host = 'localhost';
-$db = 'raspadinha';
-$user = 'raspadinha';
-$pass = 'raspadinha';
+$host = '186.194.55.166';
+$db = 'raspadinha777';
+$user = 'raspadinha777';
+$pass = 'Nxf7xzdcEiGBFttA';
 
 $conn = new mysqli($host, $user, $pass, $db);
 
